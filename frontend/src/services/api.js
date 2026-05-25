1e814dc69d63d718637d0d921be12c59a3260752
Qaleelsha <khaleelshabacker123@gmail.com>
2026-05-25 15:35:38 +0530

    Fixed AWS frontend deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://13.53.197.54:8000",
 });
 
 API.interceptors.request.use((req) => {
